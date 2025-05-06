eca75ba451a2844dc0a55a6b64451c630430883c
Gabriel Moura <gabrielmouraintj@gmail.com>
2025-05-06 11:28:38 -0300

    ...

--- 01-call.py
+++ 01-call.py
@@ -1,6 +1,14 @@
+from operator import truediv
+
 from exercicio import Pessoa
 
 aluno01 = Pessoa(nome="Gabriel", idade=21, peso=106)
 print(aluno01.nome)
 print(aluno01.idade)
 print(aluno01.peso)
+
+aluno01.falar()
+aluno01.comer()
+aluno01.dormir()
+
+
--- exercicio.py
+++ exercicio.py
@@ -3,3 +3,14 @@ class Pessoa():
         self.nome=nome
         self.idade=idade
         self.peso=peso
+        self.falando=False
+        self.comendo=False
+        self.dormindo=False
+    def falar(self):
+        print(f"{self.nome} começoua a falar")
+    def comer(self, comida):
+        if self.comendo == True
+        print(f"{self.nome} comeu {comida}")
+    def dormir(self):
+        print(f"{self.nome} dormiu ")
+
